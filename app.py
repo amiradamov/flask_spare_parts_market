9d41b973400e17492a596b3431f661698df6ec27
amiradamov <adamov.aamir@gmail.com>
2023-06-17 17:55:04 +0400

    new commit

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from werkzeug.security import generate_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "my super secret key"
+# Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField("What's Your Name", validators = [DataRequired()])
+    submit = SubmitField('Submit')
 
 # FILTERS!!!
 
@@ -28,6 +35,21 @@ def index():
 def user(name):
     return render_template('user.html', name = name)
 
+#Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+
+    return render_template("name.html",
+        name = name,
+        form = form)
+
 # Create Custom Error Pages
 
 # Invalid URL
@@ -39,6 +61,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
 
 if __name__ == "__main__":
     app.run(debug = True)
